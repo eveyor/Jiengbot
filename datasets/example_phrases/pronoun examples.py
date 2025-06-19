908aa58986d635c49cac64d2fa20bba7489ef824
eveyor <eveyor5@gmail.com>
2025-06-19 22:14:12 +1000

    Create pronoun examples.py

---
+++ datasets/example_phrases/pronoun examples.py
@@ -0,0 +1,13 @@
+##example phrases
+example_pronoun = {
+"I will come"  :  "yen bi ben"
+"Tell me"  :  "lek yen"
+"Yin e wic piu"  :  "You want water"
+"She"  :  "Yen a daal"
+"He"  :  "Yen a daal"
+"Tell him to go"  :  "Lek e yen bi jal"
+"We are Sleeping"  :  "yook aa niin"
+"Talk to us"  :  "jam e ke yook"
+"They are waiting"  :  "Keek aa tit" 
+"Teach them"  :  "Pioc e keek"
+}
