8a1dd52a0cad0b793769f6d97b454d7954bf96ec
eveyor <eveyor5@gmail.com>
2025-06-19 12:37:59 +1000

    Create life events.py

---
+++ datasets/life events.py
@@ -0,0 +1,16 @@
+#Dinka,English
+dinka_lifeevents = {
+"Life Events"  :  "Kang rot looi e piir yic"
+"Birth"  :  "Dhiet/Dhieth"
+"Start School"  :  "Jok Pan Piooc"
+"Graduate"  :  "Thok"
+"Get a Job"  :  "Goor (wic) Luoi"
+"Retire"  :  "Muol Luoi"
+"Move"  :  "Kok"
+"Get Sick"  :  "Aci Tuaany"
+"Death"  :  "Thuoou"
+"Marriage"  :  "Thieek"
+"Love"  :  "Nhier"
+"Divorce"  :  "Lioi"
+"To have a baby"  :  "Ba Nang Meth"
+}
