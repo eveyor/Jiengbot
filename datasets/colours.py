2cd2200de47fdeb99fc60185b57f5009ffb80cbf
eveyor <eveyor5@gmail.com>
2025-06-19 12:41:29 +1000

    Create colours.py

---
+++ datasets/colours.py
@@ -0,0 +1,12 @@
+#Dinka, English
+dinka_colours = {
+"Colours"  :  "Kit"
+"Red"  :  "Malual"
+"Yellow"  :  "Mayen"
+"Grey"  :  "Malith"
+"Green"  :  "Mangok loc"
+"Brown"  :  "Mathiang"
+"Blue"  :  "Mangok"
+"Black"  :  "Macar"
+"White"  :  "Mabior"
+}
