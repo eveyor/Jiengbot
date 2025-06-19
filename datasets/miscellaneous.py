a20dc06c85f7b947b083eabe3feb5de8da352fc8
eveyor <eveyor5@gmail.com>
2025-06-19 13:09:30 +1000

    Create miscellaneous.py

---
+++ datasets/miscellaneous.py
@@ -0,0 +1,17 @@
+#dina, misc
+dinka_miscellaneous = {
+"Plant Life"  :  "Piir Tiim"
+"Flower"  :  "Yook"
+"Trees"  :  "Tiim"
+"Lead"  :  "Yaar"
+"Branch"  :  "Keer"
+"Root"  :  "Meei"
+"Health and Medical"  :  "Pial Ku wel"
+"Nurse"  :  "Raan Muk Koc Tuany"
+"Patient"  :  "Raan Tuany"
+"Cruches"  :  "Wei e Kom"
+"Syringe"  :  "Winh e Tuom"
+"Stethoscope"  :  "Thethothkop"
+"Medicine"  :  "wal"
+"Blanket"  :  "Parany Thiek"
+}
