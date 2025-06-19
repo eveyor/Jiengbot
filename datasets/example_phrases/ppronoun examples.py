a77e3de0036b2d8842b15c5b6ae5c38b30ad3a5f
eveyor <eveyor5@gmail.com>
2025-06-19 22:23:04 +1000

    Create ppronoun examples.py

---
+++ datasets/example_phrases/ppronoun examples.py
@@ -0,0 +1,10 @@
+##possessive pronoun examples  
+ppronoun_examples = {
+"This book is mine"  :  "Athor kene e kedi3"
+"This is my friend"  :  "kene e mathdi3"
+"That school is ours"  :  "Pan abun tui e keda"
+"It is yours"  :  "E kedu"
+"This spoon is hers"  :  "Thiaal kene e kede"
+"This pot is theirs"  :  "Tony kene e kede"  
+"This is mine"  :  "kene e kedi3"
+}
