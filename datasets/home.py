86a1d91edab7db75122b34ec5dd4da02f6ff9c8c
eveyor <eveyor5@gmail.com>
2025-06-19 11:33:18 +1000

    Create home.py

---
+++ datasets/home.py
@@ -0,0 +1,17 @@
+##dinka,english
+dinka_home = {
+"Home"  :  "Baai"
+"Window"  :  "Aluiit"
+"Roof"  :  "Yot Nhom"
+"House"  :  "Yot"
+"Front Door"  :  "Yot Thok Tueng"
+"Driveway"  :  "Kueth"
+"Backyard"  :  "Yot Kou"
+"Kitchen"  :  "Yon-That"
+"Bedroom"  :  "Yon-Nin"
+"Stairs"  :  "Laang"
+"Bathroom"  :  "Yon-Laak"
+"Floor"  :  "Piny"
+"Shower"  :  "Yon Laak"
+"Toilet"  :  "Yon La Roor"
+}
