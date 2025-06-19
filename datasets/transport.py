80a39b370d7a385575ce800adbba7b7119648be0
eveyor <eveyor5@gmail.com>
2025-06-19 11:28:08 +1000

    Create transport.py

---
+++ datasets/transport.py
@@ -0,0 +1,15 @@
+##English,Dinka
+dinka_transport = {
+"Transport"  :  "Kang Cath"
+"Bicycle"  :  "Akaja Milang"
+"Motorbike"  :  "Tuktuk"
+"Aeroplane"  :  "Rian Nhial"
+"Truck"  :  "Riandit"
+"Car"  :  "Rian Koor"
+"Taxi"  :  "Rian Ye Riopic"
+"Boat"  :  "Rian Wiir"
+"Train"  :  "Turen"
+"Train Station"  :  "Ten Kooc Turen"
+"Bus"  :  "Bath"
+"Bus Stop"  :  "Ten Kooc Bath"
+}
