20d56dca8e693767c308ca1bc78f9b0a5e7cdb3e
eveyor <eveyor5@gmail.com>
2025-06-19 12:23:30 +1000

    Create housework.py

---
+++ datasets/housework.py
@@ -0,0 +1,19 @@
+##dinka,english
+dinka_housework = {
+"Housework"  :  "Luoi baai"
+"Dust the Furniture"  :  "Teng Tang Nhiim"
+"Make the Bed"  :  "Thiet Agerem"
+"Cook the dinner"  :  "Thal Mieth (Thei)"
+"Wash the Dishes"  :  "Lok Aduuk Yiic"
+"Dry the Dishes"  :  "Wuny Aduuk"
+"Wash the Windows"  :  "Lok Aluit Nyin"
+"Do the Laundry"  :  "Lok Aleith"
+"Clean the Bathroom"  :  "Lok Yon Laak"
+"Sweep the Floor"  :  "Kut Piny"
+"Rake the Leaves"  :  "Kuar Yith E Tiim"
+"Vacuum the Carpet"  :  "Koth Karpet Yic"
+"Water the Plants"  :  "Yor Tiim"
+"Mow the Lawn"  :  "Ner Noon"
+"Plant a Tree"  :  " Com Tim"
+"Take out the Garbage"  :  "Yath anyuoon ayeer"
+}
