efd4c9cb1e85afc6e9484c1ab9b5448ce854108e
eveyor <eveyor5@gmail.com>
2025-06-19 21:28:22 +1000

    Create pronouns.py

---
+++ datasets/pronouns.py
@@ -0,0 +1,13 @@
+##Dinka pronouns
+dinka_pronouns = {
+"You"  :  "Week"
+"You"  :  "Yin e rot"
+"They"  :  "Keek"
+"Them"  :  "Keek/ke"
+"Us"  :  "wook/yook"
+"We"  :  "yook/yo"
+"She/He/It"  :  "Yen"
+"You"  :  "Yin/yi"
+"Me"  :  "yen"
+"I"  :  "yen/ya"
+}
