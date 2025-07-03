29856780b4f3525010da5b2ba86adace3da28386
eveyor <eveyor5@gmail.com>
2025-07-03 13:48:06 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import random
 
+## I need to import my datasets so that I have something to generate coherent sentences from
+from datasets import verbs
+from datasets import pronouns
+from datasets import possessive_pronouns
+
+## I need to make my function
+def main ()
+
+## I will implement a code that is supposed to make it easier to locate the specific words in my dataset
+
+##I am going to utilise a simple function that utilises Natural Language Processing (NLP) techniques to generate a sentence in Dinka language.
